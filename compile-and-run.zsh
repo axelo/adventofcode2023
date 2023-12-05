12f6ed797ae3f363a27f0001744f7cd247c3b8e3
Axel Olsson <axel.olsson@insurello.se>
2023-12-05 08:08:40 +0100

    tweaks

--- compile-and-run.zsh
+++ compile-and-run.zsh
@@ -3,7 +3,23 @@
 set -euo pipefail
 
 DAY="$1"
-INPUT="$2"
+
+if [[ -z ${2-} ]]; then
+    INPUT="${1:0:-1}.input"
+else
+    INPUT=$2
+fi
+
+FLAGS="-Ofast \
+    -Wunused-but-set-variable \
+    -Wunused-parameter \
+    -Wunused-macros"
+
+if [[ -v D ]]; then
+    FLAGS="-O0 \
+        -Wno-unused-function \
+        -Wno-unused-variable"
+fi
 
 mkdir -p build
 
@@ -19,10 +35,7 @@ clang \
     -Wsign-compare \
     -Wswitch-enum \
     -Wno-gnu-binary-literal \
-    -Wunused-variable \
-    -Wunused-function \
-    -Wunused-but-set-variable \
-    -Wunused-parameter \
+    ${=FLAGS} \
     -std=c17 \
     --debug \
     -o "./build/$DAY" \
--- template.c
+++ template.c
@@ -5,10 +5,11 @@
 int main(void) {
     char *line = NULL;
     size_t ngetline = 0;
+
     int nchars = 0;
 
     while ((nchars = (int)getline(&line, &ngetline, stdin)) > 0) {
-        if (line[nchars - 1] == '\n') line[--nchars] = '\0';
+        if (line[nchars - 1] == '\n') line[--nchars] = 0;
 
         printf("line: %s\n", line);
     }
